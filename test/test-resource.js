321ab3ef8cfe55c33ab61c4f3d779cf94626bae3
elementsweb <anon_shop@icloud.com>
2017-03-12 23:55:07 +0000

    Added mocha tests to test generator

---
+++ test/test-resource.js
@@ -0,0 +1,45 @@
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+var path = require('path');
+
+describe('concourse-resource:app', function() {
+  beforeEach(function(done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        name: 'concourse-bitbucket-status',
+        description: 'Concourse Bitbucket Status',
+        licensee_name: 'elementsweb'
+      })
+      .on('end', done);
+  });
+
+  it('the generator can be required without throwing', function() {
+    require('../generators/app');
+  })
+
+  it('has generated a README file', function() {
+    assert.file('README.md');
+    assert.fileContent('README.md', /concourse-bitbucket-status/);
+    assert.fileContent('README.md', /Concourse Bitbucket Status/);
+    assert.fileContent('README.md', /2017 elementsweb/);
+  });
+
+  it('has generated a LICENSE file', function() {
+    assert.file(['LICENSE']);
+  });
+
+  it('has generated a Dockerfile and .dockerignore', function() {
+    assert.file([
+      'Dockerfile',
+      '.dockerignore'
+    ]);
+  });
+
+  it('has generated the scripts', function() {
+    assert.file([
+      'scripts/check',
+      'scripts/in',
+      'scripts/out'
+    ]);
+  });
+});
